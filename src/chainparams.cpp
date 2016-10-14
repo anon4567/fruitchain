e25e3870958953ea61bb0cd023b14f1f54772d4f
Yuncong Hu <huyuncongh@163.com>
2016-10-14 19:27:13 -0400

    testnet difficulty adapted

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -164,8 +164,11 @@ public:
         consensus.BIP65Height = 581885; // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
         consensus.BIP66Height = 330776; // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
 //        consensus.powLimit = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-        //verFruit
-        consensus.powLimit = uint256S("0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        //verFruit 
+        /** if powlimit is too small, fruit difficulty will be equal to block difficulty,
+         *  and may be not able to get fruit
+        **/
+        consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");       
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = 10 * 60;
         consensus.fPowAllowMinDifficultyBlocks = true;
