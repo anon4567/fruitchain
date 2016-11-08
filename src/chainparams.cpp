47dd8ebf53be40cf68088b17b4282c0f7b4296e6
Yuncong Hu <huyuncongh@163.com>
2016-11-08 17:11:30 -0500

    diff change

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -169,9 +169,9 @@ public:
         /** if powlimit is too small, fruit difficulty will be equal to block difficulty,
          *  and may be not able to get fruit
         **/
-        consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-        consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
-        consensus.nPowTargetSpacing = 10 * 60;
+        consensus.powLimit = uint256S("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); //uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        consensus.nPowTargetTimespan = 60; //14 * 24 * 60 * 60; // two weeks
+        consensus.nPowTargetSpacing = 60; //10 * 60;
         consensus.fPowAllowMinDifficultyBlocks = true;
         consensus.fPowNoRetargeting = false;
         consensus.nRuleChangeActivationThreshold = 1512; // 75% for testchains
