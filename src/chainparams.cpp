b2d97736189a5c5a5231d159b52e9c5c9d78b5b4
Yuncong Hu <huyuncongh@163.com>
2016-11-11 02:43:14 -0500

    change difficult

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -170,12 +170,12 @@ public:
          *  and may be not able to get fruit
         **/
         consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-        consensus.nPowTargetTimespan = 60; //14 * 24 * 60 * 60; // two weeks
-        consensus.nPowTargetSpacing = 30; //10 * 60;
+        consensus.nPowTargetTimespan = 16 * 3 * 60//14 * 24 * 60 * 60; // two weeks
+        consensus.nPowTargetSpacing = 3 * 60; //10 * 60;
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
         consensus.nRuleChangeActivationThreshold = 1512; // 75% for testchains
-        consensus.nMinerConfirmationWindow = 2; //2016;       // nPowTargetTimespan / nPowTargetSpacing
+        consensus.nMinerConfirmationWindow = 16; //2016;       // nPowTargetTimespan / nPowTargetSpacing
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999;   // December 31, 2008
